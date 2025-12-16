f42366c33860b19529262acee4671c66ff3f1ccd
Urvashi Singh <us229253@gmail.com>
2025-12-16 23:45:40 +0530

    reset change

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-/*
+
 import illustration from "@/assets/04.jpeg"
 import bgImage from "@/assets/9.png"      // ya 10.jpeg jo bhi use karna ho
 import FormComponent from "@/components/forms/FormComponent"
@@ -23,61 +23,10 @@ function HomePage() {
                 </div>
             </div>
 
-            {/* <Footer /> *///}
-//         </div>
-//     )
-// }
+            {/* <Footer /> */}
+         </div>
+     )
+ }
 
-// export default HomePage
+ export default HomePage
 
-import illustration from "@/assets/04.jpeg"
-import bgImage from "@/assets/9.png"
-import FormComponent from "@/components/forms/FormComponent"
-
-function HomePage() {
-    return (
-        <div className="relative min-h-[100svh] w-full overflow-hidden bg-black">
-            {/* Background image layer */}
-            <img
-                src={bgImage}
-                alt=""
-                className="absolute inset-0 h-full w-full object-cover"
-                loading="eager"
-            />
-
-            {/* Content layer */}
-            <div className="relative z-10 flex min-h-[100svh] flex-col items-center justify-center text-white">
-                <div
-                    className="
-                        mx-auto
-                        flex 
-                        w-full 
-                        max-w-7xl
-                        flex-col 
-                        items-center 
-                        justify-between 
-                        gap-12
-                        px-6
-                        sm:flex-row
-                    "
-                >
-                    {/* Left Illustration */}
-                    <div className="flex w-full justify-center sm:w-1/2">
-                        <img
-                            src={illustration}
-                            alt="CodeVerse Collaboration Illustration"
-                            className="w-[260px] sm:w-[420px]"
-                        />
-                    </div>
-
-                    {/* Right Form */}
-                    <div className="flex w-full justify-center sm:w-1/2">
-                        <FormComponent />
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default HomePage
